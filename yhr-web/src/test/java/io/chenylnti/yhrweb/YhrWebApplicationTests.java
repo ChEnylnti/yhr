7da4f8b230e08ec72a38f96d873b774ed3408197
chenylnti <2374957923@qq.com>
2024-04-05 21:55:06 +0800

    初始化项目结构

---
+++ yhr-web/src/test/java/io/chenylnti/yhrweb/YhrWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chenylnti.yhrweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhrWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
